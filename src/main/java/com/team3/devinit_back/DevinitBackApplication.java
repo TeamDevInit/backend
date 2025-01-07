a696a74639f00ab57f49254633c55014781d930c
moonsunmeane <anstjsals4078@gmail.com>
2025-01-07 19:22:38 +0900

    Initial commit

---
+++ src/main/java/com/team3/devinit_back/DevinitBackApplication.java
@@ -0,0 +1,13 @@
+package com.team3.devinit_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevinitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevinitBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team3/devinit_back/DevinitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.devinit_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevinitBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
