adcc01db3dc5daa09c6b29244adadef4126f261a
Jiwon <jiwon5568@gmail.com>
2025-01-13 16:50:49 +0900

    Fix: 사용자 이름 필드 삭제, profile, resume 연관관계 추가

--- src/main/java/com/team3/devinit_back/member/entity/MemberEntity.java
+++ src/main/java/com/team3/devinit_back/member/entity/MemberEntity.java
@@ -1,8 +1,12 @@
 package com.team3.devinit_back.member.entity;
 
+import com.team3.devinit_back.profile.entity.Profile;
+import com.team3.devinit_back.resume.entity.Resume;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -12,7 +16,6 @@ public class MemberEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-    //private String name;
 
     @Column(nullable = false, unique = true)
     private String nickName;
@@ -21,5 +24,11 @@ public class MemberEntity {
     private String socialId;
     private String socialProvider;
     private String role;
+    private String profileImage;
 
+    @OneToOne(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Profile profile;
+
+    @OneToOne(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Resume resume;
 }
