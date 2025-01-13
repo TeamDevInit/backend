631bb7c7537fd92ab24f32fecf20e225feda714b
Jiwon <jiwon5568@gmail.com>
2025-01-13 13:25:28 +0900

    Feat: Profile 엔티티 생성

---
+++ src/main/java/com/team3/devinit_back/profile/entity/Profile.java
@@ -0,0 +1,42 @@
+package com.team3.devinit_back.profile.entity;
+
+import com.team3.devinit_back.member.entity.MemberEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Profile")
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private MemberEntity member;
+
+    @ManyToOne
+    @JoinColumn(name = "grade_id", nullable = false)
+    private Grade grade;
+
+    @Column(name = "profile_image", length = 2000)
+    private String profileImage;
+
+    @Column(name = "about", length = 255)
+    private String about;
+
+    @Column(name = "board_cnt", nullable = false)
+    private int boardCount = 0;
+
+    @Column(name = "follower_cnt", nullable = false)
+    private int followerCount = 0;
+
+    @Column(name = "following_cnt", nullable = false)
+    private int followingCount = 0;
+
+}
