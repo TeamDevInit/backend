8bfd50800a2751af0841ad28b1b4d029c0e86342
Jiwon <jiwon5568@gmail.com>
2025-01-13 18:02:14 +0900

    Feat: 이력서 Entity 생성

---
+++ src/main/java/com/team3/devinit_back/resume/entity/Resume.java
@@ -0,0 +1,32 @@
+package com.team3.devinit_back.resume.entity;
+
+import com.team3.devinit_back.member.entity.MemberEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Resume")
+public class Resume {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @OneToOne
+    @JoinColumn(name = "memberId", nullable = false)
+    private MemberEntity member;
+
+    @Column(name = "name", length = 50)
+    private String name;
+
+    @Column(name = "position", length = 50)
+    private String position;
+
+    @Column(name = "summary", columnDefinition = "TEXT")
+    private String summary;
+
+    @Column(name = "portfolio", length = 2000)
+    private String portfolio;
+}
