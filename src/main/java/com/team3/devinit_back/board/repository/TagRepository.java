7f31dbd51bbdb9eba2d1fd384363ecabf7ddf60b
moonsunmeane <anstjsals4078@gmail.com>
2025-01-24 13:53:03 +0900

    fix: tag관련 예외 처리 추가

--- src/main/java/com/team3/devinit_back/board/repository/TagRepository.java
+++ src/main/java/com/team3/devinit_back/board/repository/TagRepository.java
@@ -4,9 +4,10 @@ import com.team3.devinit_back.board.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TagRepository  extends JpaRepository<Tag,Long> {
-    Tag findByName(String name);
-
+    Optional<Tag> findByName(String name);
     boolean existsByName(String name);
 }
--- src/main/java/com/team3/devinit_back/board/service/TagService.java
+++ src/main/java/com/team3/devinit_back/board/service/TagService.java
@@ -3,6 +3,8 @@ package com.team3.devinit_back.board.service;
 import com.team3.devinit_back.board.entity.Tag;
 import com.team3.devinit_back.board.repository.BoardRepository;
 import com.team3.devinit_back.board.repository.TagRepository;
+import com.team3.devinit_back.global.exception.CustomException;
+import com.team3.devinit_back.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,7 +21,8 @@ public class TagService {
         return tagRepository.findAll();
     }
 
-    public Tag findTag(String name){
-        return tagRepository.findByName(name);
+    public Tag findTag(String tagName){
+        return tagRepository.findByName(tagName)
+                .orElseThrow(() -> new CustomException(ErrorCode.TAG_NOT_FOUND));
     }
 }
