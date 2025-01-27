ca58dafd13777f679c914f301ffde2185f7d0566
Jiwon <jiwon5568@gmail.com>
2025-01-27 13:21:19 +0900

    Feat: Websocket 환경 세팅 Related to Hub/Lab: #25, #15

---
+++ src/main/java/com/team3/devinit_back/global/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.team3.devinit_back.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub");
+        config.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp")
+            .setAllowedOriginPatterns("http://localhost:3000", "http://34.64.44.54", "http://kdt-pt-1-pj-1-team03.elicecoding.com", "http://localhost:5173");
+    }
+}
