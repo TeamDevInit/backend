963d3bc60ce2abbd49384e9b2963a2ca50eadfba
moonsunmeane <anstjsals4078@gmail.com>
2025-01-27 15:22:24 +0900

    feat: 이력서 요소 별 기능 추가 및 list 형태로 변환

---
+++ src/main/java/com/team3/devinit_back/resume/dto/InformationResponseDto.java
@@ -0,0 +1,36 @@
+package com.team3.devinit_back.resume.dto;
+
+import com.team3.devinit_back.resume.entity.Information;
+import com.team3.devinit_back.resume.entity.Resume;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.Data;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Data
+@Getter
+public class InformationResponseDto {
+    private Long id;
+    private String name;
+    private String position;
+    private String summary;
+    private String portfolio;
+    private String resumeId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public InformationResponseDto(Information information){
+        this.name = information.getName();
+        this.position = information.getPosition();
+        this.portfolio = information.getPortfolio();
+        this.summary = information.getSummary();
+
+        Resume resume = information.getResume();
+        if(resume != null) {this.resumeId = resume.getId();}
+
+        this.createdAt = information.getCreatedAt();
+        this.updatedAt = information.getUpdatedAt();
+    }
+    public static InformationResponseDto fromEntity(Information information) { return new InformationResponseDto(information);}
+}
