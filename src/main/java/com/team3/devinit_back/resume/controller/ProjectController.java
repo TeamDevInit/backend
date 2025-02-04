015492c1d08ee9d8e976830430edc14b28564c70
moonsunmeane <anstjsals4078@gmail.com>
2025-02-04 20:24:51 +0900

    refactor: 스웨거 설명 및 요약 작성 related to hub/lab : #43, #23

--- src/main/java/com/team3/devinit_back/resume/controller/ProjectController.java
+++ src/main/java/com/team3/devinit_back/resume/controller/ProjectController.java
@@ -10,6 +10,7 @@ import com.team3.devinit_back.resume.dto.ProjectResponseDto;
 import com.team3.devinit_back.resume.entity.Resume;
 import com.team3.devinit_back.resume.service.ProjectService;
 import com.team3.devinit_back.resume.service.ResumeService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -29,6 +30,9 @@ public class ProjectController {
 
 
     @PostMapping
+    @Operation(
+            summary = "프로젝트 이력 생성",
+            description = "사용자의 프로젝트 이력을 리스트형태로 받아 저장하고 내용을 리스트로 반환 합니다.")
     public ResponseEntity<List<ProjectResponseDto>> createProject(@AuthenticationPrincipal CustomOAuth2User userInfo,
                                                                   @Valid @RequestBody List<ProjectRequestDto> projectRequestDtos){
         Resume resume = getResumeFromUserInfo(userInfo);
@@ -38,6 +42,9 @@ public class ProjectController {
     }
 
     @PatchMapping
+    @Operation(
+            summary = "프로젝트 이력 수정",
+            description = "사용자의 수정된 프로젝트 이력을 리스트형태로 받아 저장/수정하고 내용을 리스트로 반환 합니다.")
     public ResponseEntity<ProjectResponseDto> updateProject(@AuthenticationPrincipal CustomOAuth2User userInfo,
                                                             @Valid @RequestBody List<ProjectRequestDto> projectRequestDtos){
         Resume resume = getResumeFromUserInfo(userInfo);
@@ -46,6 +53,9 @@ public class ProjectController {
     }
 
     @PutMapping
+    @Operation(
+            summary = "프로젝트 이력 생성 및 수정",
+            description = "사용자의 프로젝트 이력을 리스트형태로 받아 ID가 존재 하지 않는 부분에 대해서는 생성 있는 부분에 대해서는 수정을 진행하고 내역을 반환합니다.")
     public ResponseEntity<List<ProjectResponseDto>> saveOrUpdateProjects(@AuthenticationPrincipal CustomOAuth2User userInfo,
                                                                             @Valid @RequestBody List<ProjectRequestDto> projectRequestDtos){
         Resume resume = getResumeFromUserInfo(userInfo);
@@ -55,6 +65,10 @@ public class ProjectController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "프로젝트 이력 삭제",
+            description = "사용자의  프로젝트 이력을 삭제합니다."
+                    + "삭제할 프로젝트 이력의 ID를 경로 변수로 전달해야 합니다.")
     public  ResponseEntity<Void> deleteProject(@AuthenticationPrincipal CustomOAuth2User userInfo,
                                                @PathVariable("id") Long id){
         Resume resume = getResumeFromUserInfo(userInfo);
