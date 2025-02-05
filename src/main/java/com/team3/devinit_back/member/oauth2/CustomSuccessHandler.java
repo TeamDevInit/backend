81eaf3230ceb3e2acfab762f0eaf256fd40f588e
moonsunmeane <anstjsals4078@gmail.com>
2025-02-05 18:01:43 +0900

    test commit

--- src/main/java/com/team3/devinit_back/member/oauth2/CustomSuccessHandler.java
+++ src/main/java/com/team3/devinit_back/member/oauth2/CustomSuccessHandler.java
@@ -42,7 +42,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         // refresh -> 쿠키, access -> 헤더? -> 하이퍼링크에서 넘어오는거라 바로 못줌
         response.addCookie(createCookie("refresh", refreshToken));
-        response.sendRedirect("http://www.devinit.shop"); //  ->  http://34.64.44.54
+        response.sendRedirect("http://devinit.shop"); //  ->  http://34.64.44.54
         response.setStatus(HttpStatus.OK.value());
     }
 
